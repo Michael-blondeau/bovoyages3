fe46f3c72a4129f360e838b20f950531f3d6cf15
plassalas <plassalas@users.noreply.github.com>
2017-05-16 14:53:36 +0200

    Ajout de la gestion des dates de voyage (création)

--- BoVoyages/src/main/java/voyage/beans/DatesVoyagesBean.java
+++ BoVoyages/src/main/java/voyage/beans/DatesVoyagesBean.java
@@ -3,10 +3,16 @@ package voyage.beans;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
-@Named("dates")
+import voyage.entities.DatesVoyages;
+import voyage.entities.Destination;
+import voyage.services.CatalogueService;
+
+@Named("date")
 @ConversationScoped
 public class DatesVoyagesBean implements Serializable {
 	private static final long serialVersionUID = 357912718931065193L;
@@ -17,16 +23,50 @@ public class DatesVoyagesBean implements Serializable {
 	private double prix;
 	private int nbVoyageurs;
 	
+	private Destination destination;
+	
+	@Inject
+	private Conversation conversation;
+	
+	@Inject
+	private CatalogueService service; 
+	
 	public DatesVoyagesBean() {	}
 	
 	public DatesVoyagesBean(Date dateDepart, Date dateRetour, double prix, int nbVoyageurs) {
-		super();
 		this.dateDepart = dateDepart;
 		this.dateRetour = dateRetour;
 		this.prix = prix;
 		this.nbVoyageurs = nbVoyageurs;
 	}
+	
+	public String addDate(int id){
+		startConversation();
+		this.destination = service.getDestinationById(id);
+		return "creationDateVoyage?faces-redirect=true";
+	}
+	
+	public String add(){
+		DatesVoyages dv = new DatesVoyages(dateDepart, dateRetour, prix, nbVoyageurs);
+		destination.addDate(dv);
+		service.saveOrUpdate(destination);
+		stopConversation();
+		return "allDestinations?faces-redirect=true";
+	}
 
+	
+	public void startConversation(){
+		if (conversation.isTransient()) {
+			conversation.begin();
+		}	
+	}
+	
+	public void stopConversation(){
+		if (!conversation.isTransient()) {
+			conversation.end();
+		}
+	}
+	
 	public int getId() {
 		return id;
 	}
